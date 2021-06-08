094d69f01a3273a15a7d7f55749f416f283c5bce
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-06-08 15:22:05 +0530

    Update typewise_alert.py
    final checkin

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,15 +1,9 @@
-Target_types_of_alret = {"TO_CONTROLLER": send_to_controller, "TO_EMAIL" : send_to_email, "TO_CONSOLE" : send_to_console}
-
 cooling_types = {'PASSIVE_COOLING' : {'min': 0, 'max': 35}, 'HIGH_ACTIVE_COOLING' : {'min': 0, 'max': 45}, 'MED_ACTIVE_COOLING' : {'min': 0, 'max': 40}}
 
-threshold_limit_msgs = {'TOO_LOW' : 'too low', 'TOO_HIGH' : 'too high', 'NORMAL' : 'normal'}
-
-breachType_email = {
-        'TOO_LOW': {'recepient': "Toolow.b@c.com"},
-        'TOO_HIGH': {'recepient': "TooHigh.b@c.com"},
-        'NORMAL': {'recepient': "TooNormal.b@c.com" }
-}
-
+alert_messages_for_breach_code = { 'TOO_LOW'  : 'Temperature is too low', 
+                                   'TOO_HIGH' : 'Temperature is too high', 
+                                   'NORMAL'   : 'Temperature is normal' }
+                                
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
     return 'TOO_LOW'
@@ -17,31 +11,31 @@ def infer_breach(value, lowerLimit, upperLimit):
     return 'TOO_HIGH'
   return 'NORMAL'
 
+
 def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if (coolingType in cooling_types and temperatureInC is not None):
-    range = cooling_types[coolingType]
-    lowerLimit = range['min']
-    upperLimit = range['max']
-    return infer_breach(temperatureInC, lowerLimit, upperLimit)
-  else:
-    return 'Wrong Input'
+  lowerLimit = cooling_types[coolingType]['lowerLimit']
+  upperLimit = cooling_types[coolingType]['upperLimit']
+  return infer_breach(temperatureInC, lowerLimit, upperLimit)
 
 def send_to_controller(breachType):
   header = 0xfeed
-  print(f'{header}, {breachType}')
-  
+  print(f'{header}, {breachType}')                             
+  return f'{header}, {breachType}'
+
 def send_to_console(breachType):
-  print(f'BreachType is:, {breachType}')
-  return True
+  header = 0xfeed
+  print(f'{header}, {breachType}')                             
+  return f'{header}, {breachType}'
 
 def send_to_email(breachType):
-   print(f"Hello: {breachType_email[breachType]['recepient']}\n Breach is: {threshold_limit_msgs[breachType]}")
-   return True
+  recepient = "a.b@c.com"
+  print(f'To: {recepient}')
+  print(alert_messages_for_breach_code[breachType]) 
+  return alert_messages_for_breach_code[breachType]                               
 
+alert_target_function_mapping = { "TO_CONTROLLER": send_to_controller, "TO_EMAIL" : send_to_email, "TO_CONSOLE" : send_to_console }
 
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType = classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  return (Target_types_of_alret[alertTarget](breachType))
+  breachType = classify_temperature_breach(batteryChar, temperatureInC)
+  return alert_target_function_mapping[alertTarget](breachType)
 
