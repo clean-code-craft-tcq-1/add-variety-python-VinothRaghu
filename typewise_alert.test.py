501d0d50175208f3026c318d2a9811f8285188e1
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-06-08 15:14:24 +0530

    Update typewise_alert.test.py
    checkin and deliver

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -4,8 +4,9 @@ import typewise_alert
 
 class TypewiseTest(unittest.TestCase):
   def test_check_and_alert(self):
-    self.assertTrue(typewise_alert.check_and_alert('console',{'coolingType':'PASSIVE_COOLING'},70)== True)
-    self.assertTrue(typewise_alert.check_and_alert('email',{'coolingType':'PASSIVE_COOLING'},70)== True)
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL',{'coolingType':'PASSIVE_COOLING'},70)== True)
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER',{'coolingType':'PASSIVE_COOLING'},70)== True)
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONSOLE',{'coolingType':'PASSIVE_COOLING'},70)== True)
 
 
 if __name__ == '__main__':
